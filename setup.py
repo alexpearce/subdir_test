755272af7398cfe8c675ceee396632026be08bda
Alex Pearce <alex@alexpearce.me>
2014-07-30 12:34:37 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='subdir_test',
+    version='0.0.1',
+    description='A test of subdirectory module installation with pip.',
+    author='Alex Pearce',
+    url='https://github.com/alexpearce/subdir_test',
+    packages=['subdir_test']
+)
